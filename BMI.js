c697e9d814dea6343a64dbb5c82b9096e2fa5f2f
NatZuKix <88130028+NatZuKix@users.noreply.github.com>
2022-09-03 23:52:44 +0700

    Add files via upload

--- BMI.js
+++ BMI.js
@@ -1,5 +1,5 @@
 function calculateBMI(weight, height) {
-    return weight/(height*height);   // The function returns the product of p1 and p2
+    return weight/(height*height);  
   }
 function getBMIMeaning(weight, height){
     let x=calculateBMI(weight,height);
@@ -13,6 +13,3 @@ function getBMIMeaning(weight, height){
 }
 
 
-console.log(getBMIMeaning(65,1.8));
-console.log(getBMIMeaning(80,1.7));
-console.log(getBMIMeaning(44,1.6));
